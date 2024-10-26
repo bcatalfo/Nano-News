23b639a9049b737c9bce541aeb7f439cd5fc9366
Benjamin Catalfo <bcatalfo@gmail.com>
2024-10-25 21:39:05 -0400

    Now can insert multiple bullet points.

--- xerg.js
+++ xerg.js
@@ -1,7 +1,13 @@
 console.log("THis is xerg speaking");
 const article = document.getElementsByName("articleBody")[0];
 console.log("article is " + article);
+const heading = document.createElement("h1");
+heading.textContent = "Generating summary...";
 // This is gonna be in a for loop
 const summary = document.createElement("p");
 summary.textContent = "This is an ai generated bullet point";
-article.prepend(summary);
+article.prepend(heading, summary);
+
+const nextLine = document.createElement("p");
+nextLine.textContent = "This is the next bullet point";
+summary.after(nextLine);
