aa92a68c5dc7cb79c3f377008e826459c30a370d
Benjamin Catalfo <bcatalfo@gmail.com>
2024-10-25 21:31:00 -0400

    Inserted a paragraph

--- xerg.js
+++ xerg.js
@@ -1,1 +1,7 @@
 console.log("THis is xerg speaking");
+const article = document.getElementsByName("articleBody")[0];
+console.log("article is " + article);
+// This is gonna be in a for loop
+const summary = document.createElement("p");
+summary.textContent = "This is an ai generated bullet point";
+article.prepend(summary);
